fde25ebd09a59270f7dc439586ee2e01c1d8f6c1
Lucas Campos <lucasgcampos.contato@gmail.com>
2021-06-01 21:07:47 -0300

    Update some libraries

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -1,10 +1,11 @@
 private object Versions {
     const val gradleVersion = "4.0.0"
-    const val kotlinVersion = "1.3.70"
-    const val androidxVersion = "1.1.0"
-    const val constraintLayoutVersion = "1.1.3"
+    const val kotlinVersion = "1.5.10"
+    const val androidxVersion = "1.3.0"
+    const val recyclerViewVersion = "1.2.0"
+    const val constraintLayoutVersion = "2.0.4"
     const val retrofitVersion = "2.9.0"
-    const val navigationVersion = "2.3.0"
+    const val navigationVersion = "2.3.5"
     const val glideVersion = "4.11.0"
 }
 
@@ -20,7 +21,7 @@ object Dependency {
     const val converterGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofitVersion}"
 
     const val appCompat = "androidx.appcompat:appcompat:${Versions.androidxVersion}"
-    const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.androidxVersion}"
+    const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerViewVersion}"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
 
     const val navigationUiKtx = "androidx.navigation:navigation-ui-ktx:${Versions.navigationVersion}"
